bcfe28298982baf96c9b6de27ff1c04760d50f45
Niraj Kulkarni <kulkarniniraj14@gmail.com>
2025-08-11 22:01:03 +0530

    Add eval tests

--- evaluator_test.go
+++ evaluator_test.go
@@ -13,3 +13,51 @@ func TestEvaluate(t *testing.T) {
 		t.Errorf("Expected %f, got %f", exp_output, output.Value.(number).Value)
 	}
 }
+
+func TestEvaluateAdd(t *testing.T) {
+	initEvaluator()
+	input := "(+ 1 2)"
+	exp_output := float64(1 + 2)
+	tokens, _ := Scan(input)
+	tree, _ := parse(tokens)
+	output, _ := evaluate(tree)
+	if output.Value.(number).Value != exp_output {
+		t.Errorf("Expected %f, got %f", exp_output, output.Value.(number).Value)
+	}
+}
+
+func TestEvaluateListOps(t *testing.T) {
+	initEvaluator()
+	input := "(head (list 1 2 3))"
+	exp_output := float64(1)
+	tokens, _ := Scan(input)
+	tree, _ := parse(tokens)
+	output, _ := evaluate(tree)
+	if output.Value.(number).Value != exp_output {
+		t.Errorf("Expected %f, got %f", exp_output, output.Value.(number).Value)
+	}
+}
+
+func TestEvaluateListOps2(t *testing.T) {
+	initEvaluator()
+	input := "(tail (list 1 2 3))"
+	exp_output := parserToken{Type: PARSER_LIST, Children: []parserToken{{Type: PARSER_SYMBOL, Value: number{Value: 2}}, {Type: PARSER_SYMBOL, Value: number{Value: 3}}}}
+	tokens, _ := Scan(input)
+	tree, _ := parse(tokens)
+	output, _ := evaluate(tree)
+	if len(output.Children) != len(exp_output.Children) {
+		t.Errorf("Expected %f, got %f", exp_output, output.Value.(number).Value)
+	}
+}
+
+func TestEvaluateListOps3(t *testing.T) {
+	initEvaluator()
+	input := "(join (list 1 2 3) (list 4 5 6))"
+	exp_output := parserToken{Type: PARSER_LIST, Children: []parserToken{{Type: PARSER_SYMBOL, Value: number{Value: 1}}, {Type: PARSER_SYMBOL, Value: number{Value: 2}}, {Type: PARSER_SYMBOL, Value: number{Value: 3}}, {Type: PARSER_SYMBOL, Value: number{Value: 4}}, {Type: PARSER_SYMBOL, Value: number{Value: 5}}, {Type: PARSER_SYMBOL, Value: number{Value: 6}}}}
+	tokens, _ := Scan(input)
+	tree, _ := parse(tokens)
+	output, _ := evaluate(tree)
+	if len(output.Children) != len(exp_output.Children) {
+		t.Errorf("Expected %f, got %f", exp_output, output.Value.(number).Value)
+	}
+}
